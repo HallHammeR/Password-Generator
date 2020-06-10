dc8886ddd8fe21a2006b3a1c6d2d4b9104577436
josh hall <HallHammR@gmail.com>
2020-06-10 13:59:02 -0400

    added slider

--- script.js
+++ script.js
@@ -134,3 +134,11 @@ function randomChar(arr) {
 	console.log("random character: " + randomlySelectedCharacter);
 	return randomlySelectedCharacter;
 }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+	output.innerHTML = this.value;
+};
